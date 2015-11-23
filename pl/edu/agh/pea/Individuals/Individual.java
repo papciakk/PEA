4ec0c6f08c2fc5967514688c74460c4fe6d97930
CharlieBr <kasia_glab@wp.pl>
2015-11-23 14:29:35 +0100

    Update Individual.java
    change class structure

--- pl/edu/agh/pea/Individuals/Individual.java
+++ pl/edu/agh/pea/Individuals/Individual.java
@@ -4,55 +4,70 @@ import java.util.Random;
 import pl.edu.agh.pea.Core.ProblemParameters;
 
 public class Individual implements Cloneable {
-	public double [] x;
+	private double [] genotype;
 	private double fitness;
-	
-	private int dimmensions;
-	
+	private int dimmension;
 	private double ACoefficient;
-	
 	private Random rand;
 	
-	public Individual (ProblemParameters problemParameters, boolean init) {
+	public Individual (ProblemParameters problemParameters) {
 		this.dimmensions = problemParameters.dimensions;
 		this.ACoefficient = problemParameters.ACoefficient;
-		x = new double [dimmensions];
-		
-		if(init) {
-			rand = new Random();
-			randomPopulation ();
-		}
+		genotype = new double [dimmensions];
+		setFitness();
 	}
-	
-	public void randomPopulation () {
-		for (int i = 0; i < dimmensions; i++) {
-			x[i] = rand.nextDouble() * 10.24 - 5.12;
-		}
-	}
-	
-	public void printPopulation () {
-		updateFitness();
-		System.out.println("Fitness: " + String.format("%.3f", getFitness()));
-		
-		System.out.print("Genome: [");
-		for(double ch : x) {
-			System.out.print(String.format("%.3f", ch) + " ");
-		}
-		System.out.println("]");
-	}
-	
-	public void updateFitness() {		
+
+	public void setFitness(){
 		fitness = ACoefficient*dimmensions;
+
 		for(int i = 0; i < dimmensions; i++) {
-			fitness += x[i]*x[i] - ACoefficient*Math.cos(2.0*Math.PI*x[i]); 
+			fitness += x[i]*x[i] - ACoefficient*Math.cos(2.0*Math.PI*x[i]);
 		}
 	}
-	
+
+	public void updateFitness() {
+		//TODO - aktualizuje fitness
+	}
+
 	public double getFitness() {
 		return fitness;
 	}
+
+	public void updateGenotype(){
+		//TODO - aktualizuje genotyp osobnika
+	}
+
+	public void updateGen(int n){
+		//TODO - zmienia konkretny gen
+	}
+
+	public void randomGenotype(){
+		//TODO - zwraca losowy genotyp osobnika
+	}
+
+	public void setGen(){
+		//TODO - ustawia gen
+	}
+
+	public [] double getGenotype(){
+		//TODO - zwraca tablice z genotypem
+	}
+
+	public double getGen(int n){
+		//TODO - zwraca gen o indeksie n
+	}
+
+	public void printIndividual () {
+		System.out.print("Individual with genotype: [");
+
+		for(double g : genotype) {
+			System.out.print(String.format("%.3f", g) + " | ");
+		}
+
+		System.out.println("]");
+
+		System.out.println("has fitness: " + String.format("%.3f", getFitness()));
+	}
 	
-	public Individual clone() throws CloneNotSupportedException {
-        return (Individual) super.clone();
-    }
+
 }

45432afea5debf66dafdfc68fe0183dcc2acac01
papciakk <papciakk@student.agh.edu.pl>
2015-12-05 13:28:26 +0100

    Structural changes

--- pl/edu/agh/pea/Operators/ICrossover.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.pea.Operators;
-
-import pl.edu.agh.pea.Core.ProblemParameters;
-import pl.edu.agh.pea.Individuals.Individual;
-
-public interface ICrossover extends IOperator{
-	public Individual getResults();
-	void setInput(Individual input1, Individual input2, ProblemParameters problemParameters);
-}

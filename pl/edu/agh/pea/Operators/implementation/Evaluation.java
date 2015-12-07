25e79db950d71150bab84a76efde77c58c19d6da
mmorawie <mikolaj.morawiec@gmail.com>
2015-12-07 06:46:45 +0100

    nie wiem czy to dziala

--- pl/edu/agh/pea/Operators/implementation/Evaluation.java
+++ pl/edu/agh/pea/Operators/implementation/Evaluation.java
@@ -6,9 +6,23 @@ import pl.edu.agh.pea.Core.ProblemParameters;
 import pl.edu.agh.pea.Individuals.Individual;
 import pl.edu.agh.pea.Operators.IOperator;
 
+interface Fnc<T, R> {
+	R apply(T x);
+} 
+
 public class Evaluation implements IOperator {
 
 	private List<Individual> population;
+
+	Fnc<Individual, Double> lt_1 = (x) -> {
+		double fitness;
+		double gene;
+		for(int i = 0; i < ProblemParameters.dimensions; i++) {
+			gene = individual.getGen(i);
+			fitness += gene*gene - ProblemParameters.ACoefficient*Math.cos(2.0*Math.PI*gene);
+		}
+		return fitness;
+	};
 
 	public void execute() {
 		for(Individual individual : population) {

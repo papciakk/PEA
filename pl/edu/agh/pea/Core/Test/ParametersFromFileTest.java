06d37541b88d403d5c7ccdd551640397958e9af5
Marcin Górka <unimator@gazeta.pl>
2015-12-12 00:12:15 +0100

    Tests for Core added

---
+++ pl/edu/agh/pea/Core/Test/ParametersFromFileTest.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.pea.Core.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.edu.agh.pea.Core.ParametersFromFile;
+
+public class ParametersFromFileTest {
+
+	@Test
+	public void test() {
+		ParametersFromFile pff = new ParametersFromFile("some_file_name");
+		assertEquals("Wrong file name", pff.getFileName(), "some_file_name");
+		
+		
+	}
+
+}

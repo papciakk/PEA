048af13ff885dc5c5448701265bfdcfb6b4ea70f
papciakk <papciakk@student.agh.edu.pl>
2015-12-19 20:56:26 +0100

    Structural changes and new tests for Operators

--- pl/edu/agh/pea/Core/IParametersImporter.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.pea.Core;
-
-public interface IParametersImporter {
-	public boolean importParameters();
-}
---
+++ src/pl/edu/agh/pea/operators/tests/Tests.java
@@ -0,0 +1,59 @@
+package pl.edu.agh.pea.operators.tests;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import pl.edu.agh.pea.core.ProblemParameters;
+import pl.edu.agh.pea.operators.OperatorFactory;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Tests {
+
+    private static final Logger LOG = Logger.getLogger(OperatorFactory.class.getName());
+
+    private Tests() {
+    }
+
+    public static void init() {
+        initProblemParameters();
+    }
+
+    public static void initProblemParameters() {
+        ProblemParameters.dimensions = 20;
+        ProblemParameters.generations = 20;
+        ProblemParameters.population = 50;
+        ProblemParameters.mutationCoefficient = 1.0;
+        ProblemParameters.crossCoefficient = 1.0;
+        ProblemParameters.ACoefficient = 10.0;
+    }
+
+    public static void main(String[] argv) {
+        Result result;
+        boolean globalResult = true;
+
+        init();
+
+        Class[] testClasses = {
+                OperatorTest.class, OperatorFactoryTest.class,
+                CrossoverTest.class, MutationTest.class,
+                EvaluationTest.class, IndividualComparatorTest.class,
+                SelectionTest.class
+        };
+
+        String testName;
+        for (int i = 0; i < testClasses.length; i++) {
+            testName = i + ". " + testClasses[i].getName() + " Test: ";
+
+            result = JUnitCore.runClasses(testClasses[i]);
+            for (Failure failure : result.getFailures()) {
+                LOG.log(Level.SEVERE, failure.toString());
+            }
+            LOG.log(Level.INFO, testName + result.wasSuccessful());
+            globalResult &= result.wasSuccessful();
+        }
+
+        LOG.log(Level.INFO, "All tests passed: " + globalResult);
+    }
+}
